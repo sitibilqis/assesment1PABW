6c52ac939e137e1cb45ee1525cd6cbb787df517c
sitibilqis <kitcatbl@gmail.com>
2024-04-05 15:23:55 +0700

    versi1.2

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ let buku = [
     {"nama": "aladin", "jumlah":2},
   ]
 
-document.getElementById(formBuku).addEventListener("submit", function(event) {event.preventDefault});
+document.getElementById("formBuku").addEventListener("submit", function(event) {
+    event.preventDefault();
 
 // untuk mengambil nilai dari jumlah dan select option
 let jumlah = parseInt(document.getElementById("jumlah").value);
 let bukuDipilih = document.getElementById("buku").value;
+
+// mengecek apakah textfield kosong atau tidak
+if (isNaN(jumlah) || jumlah <=0) {
+    alert("Masukkan jumlah.");
+    return;
+}
+
+});
