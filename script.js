27341e06ec188eaf252e130300308bedabd2f076
sitibilqis <kitcatbl@gmail.com>
2024-04-05 15:06:17 +0700

    versi1.3

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById(formBuku).addEventListener("submit", function(event) {event.preventDefault});
 
+// untuk mengambil nilai dari jumlah dan select option
 let jumlah = parseInt(document.getElementById("jumlah").value);
 let bukuDipilih = document.getElementById("buku").value;
