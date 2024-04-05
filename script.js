ee377a63e2c597380b74c9be21b293debe99e3a5
sitibilqis <kitcatbl@gmail.com>
2024-04-05 15:37:08 +0700

    versi1.6

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let buku = [
-    {"nama": "sherlock", "jumlah":0},
+    {"nama": "sherlock", "jumlah":2},
     {"nama": "lupin", "jumlah":2},
-    {"nama": "aladdin", "jumlah":2},
   ]
 
 document.getElementById("formBuku").addEventListener("submit", function(event) {
